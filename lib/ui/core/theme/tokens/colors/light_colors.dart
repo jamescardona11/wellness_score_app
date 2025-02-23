cc3f3b3c03b988b189adc3a6f7beb2e0791c18e9
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-23 16:43:46 -0500

    🎨 Create Color Tokens

---
+++ lib/ui/core/theme/tokens/colors/light_colors.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'base_colors.dart';
+import 'utils/color_premul_lerp.dart';
+
+// Color palette
+final class LightColors extends BaseColors {
+  static LightColors def = LightColors._(
+    white: Colors.white,
+    black: Colors.black,
+  );
+
+  LightColors._({
+    required super.black,
+    required super.white,
+    Color? background,
+    Color? primary,
+    Color? foreground,
+    Color? grey50,
+    Color? grey100,
+    Color? red,
+  }) : super(
+          primary: primary ?? const Color(0xFF001C95),
+          background: background ?? const Color(0xFFE9EEF2),
+          foreground: foreground ?? const Color(0xFF1E2A32),
+          grey50: grey50 ?? const Color(0xFF4D6475),
+          grey100: grey100 ?? const Color(0xFF708797),
+          red: red ?? Colors.red.shade300,
+        );
+
+  @override
+  LightColors copyWith({
+    Color? black,
+    Color? white,
+    Color? background,
+    Color? primary,
+    Color? foreground,
+    Color? grey50,
+    Color? grey100,
+    Color? red,
+  }) {
+    return LightColors._(
+      black: black ?? this.black,
+      white: white ?? this.white,
+      background: background ?? this.background,
+      primary: primary ?? this.primary,
+      foreground: foreground ?? this.foreground,
+      grey50: grey50 ?? this.grey50,
+      grey100: grey100 ?? this.grey100,
+      red: red ?? this.red,
+    );
+  }
+
+  @override
+  BaseColors lerp(covariant ThemeExtension<BaseColors>? other, double t) {
+    if (other is! LightColors) return this;
+
+    return LightColors._(
+      black: colorPremulLerp(black, other.black, t)!,
+      white: colorPremulLerp(white, other.white, t)!,
+      background: colorPremulLerp(background, other.background, t)!,
+      primary: colorPremulLerp(primary, other.primary, t)!,
+      foreground: colorPremulLerp(foreground, other.foreground, t)!,
+      grey50: colorPremulLerp(grey50, other.grey50, t)!,
+      grey100: colorPremulLerp(grey100, other.grey100, t)!,
+      red: colorPremulLerp(red, other.red, t)!,
+    );
+  }
+}
