b1c38d773fc55a5e5e02b12966e832f336ff27cc
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-24 08:46:35 -0500

    👔  Add mock analytics implementation

--- lib/data/repositories/mock_analytics_repository.dart
+++ lib/data/repositories/mock_analytics_repository.dart
@@ -1,21 +1,61 @@
+import 'dart:developer' as developer;
+
 import 'package:wellness_score_app/domain/repositories/analytics_repository.dart';
 
 class MockAnalyticsRepository implements AnalyticsRepository {
   @override
   Future<void> trackEvent(String event, {Map<String, dynamic>? data}) async {
-    // TODO: implement trackEvent
-    throw UnimplementedError();
+    _log(event, _LogStatusInfo(), data: data);
   }
 
   @override
   Future<void> reportError({required String message, Map<String, dynamic>? data}) async {
-    // TODO: implement reportError
-    throw UnimplementedError();
+    _log(message, _LogStatusError(), data: data);
   }
 
   @override
   Future<void> trackAction(String actionName, {Map<String, dynamic>? data}) async {
-    // TODO: implement trackAction
-    throw UnimplementedError();
+    _log(actionName, _LogStatusInfo(), data: data);
   }
+
+  void _log(
+    String? text,
+    _LogStatus status, {
+    StackTrace? stackTrace,
+    String? error,
+    Map<String, dynamic>? data,
+  }) {
+    String msg = '[${status.emoji}] $text';
+
+    if (data != null) {
+      msg += '\nData $data';
+    }
+
+    if (error != null) {
+      msg += '\nError $error';
+    }
+
+    if (stackTrace != null) {
+      msg += '\nStackTrace $stackTrace';
+    }
+
+    developer.log('${status.color}$msg\x1B[0m', name: 'MockAnalytics');
+  }
+}
+
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+sealed class _LogStatus {
+  final String tag;
+  final String emoji;
+  final String color;
+
+  _LogStatus(this.tag, this.emoji, this.color);
+}
+
+class _LogStatusInfo extends _LogStatus {
+  _LogStatusInfo() : super('INFO', '✅', '\x1B[33m');
+}
+
+class _LogStatusError extends _LogStatus {
+  _LogStatusError() : super('ERROR', '😡', '\x1B[91m');
 }
