63a93ff8ecdf1775d286548ade7f608a91296c62
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-24 08:06:16 -0500

    ✨ Add cubit to health calculator screen

---
+++ lib/ui/features/health_calculator/cubit/health_calculator_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:wellness_score_app/domain/types/health_status.dart';
+import 'package:wellness_score_app/domain/use_cases/health_score_use_case.dart';
+
+part 'health_calculator_state.dart';
+
+class HealthCalculatorCubit extends Cubit<HealthCalculatorState> {
+  final HealthScoreUseCase healthScoreUseCase;
+
+  HealthCalculatorCubit({
+    required this.healthScoreUseCase,
+  }) : super(const HealthCalculatorState());
+
+  void onAnnualIncomeChanged(double value) {
+    emit(state.copyWith(annualIncome: value));
+  }
+
+  void onMonthlyCostsChanged(double value) {
+    emit(state.copyWith(monthlyCosts: value));
+  }
+
+  void onContinuePressed() {
+    if (!state.isFormValid) return;
+
+    final healthScore = healthScoreUseCase.call(
+      annualIncome: state.annualIncome,
+      monthlyCosts: state.monthlyCosts,
+    );
+
+    emit(state.copyWith(healthScore: healthScore));
+  }
+}
