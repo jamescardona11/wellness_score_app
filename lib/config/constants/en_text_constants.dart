26b409e179af440a7e0cd7da21d0ae468c150af1
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-24 08:14:48 -0500

    ✨ Add form inputs to Health Calculator Screen

--- lib/config/constants/en_text_constants.dart
+++ lib/config/constants/en_text_constants.dart
@@ -4,4 +4,7 @@ abstract class TextConstants {
   static const String financialWellnessScoreDescription = 'financial wellness score.';
   static const String financialWellnessTest = 'Financial wellness test';
   static const String financialWellnessTestDescription = 'Enter your financial information below';
+  static const String annualIncome = 'Annual income';
+  static const String monthlyCosts = 'Monthly Costs';
+  static const String continueButton = 'Continue';
 }
--- lib/ui/widgets/text/input/finance_input_field.dart
+++ lib/ui/widgets/text/input/finance_input_field.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wellness_score_app/config/assets/app_icons.dart';
 import 'package:wellness_score_app/config/constants/en_text_constants.dart';
 import 'package:wellness_score_app/ui/core/theme/theme.dart';
+import 'package:wellness_score_app/ui/widgets/text/app_text.dart';
 
 import 'currency_text_input_formatter.dart';
 
@@ -24,54 +25,62 @@ class FinanceInputField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = this.theme ?? context.theme.inputFieldTheme;
+    final sizes = context.sizes;
 
-    return SizedBox(
-      width: theme.width,
-      height: theme.height,
-      child: TextFormField(
-        maxLength: 17,
-        onChanged: (value) => onChanged(_stringValueToDouble(value)),
-        keyboardType: TextInputType.number,
-        inputFormatters: [CurrencyTextInputFormatter()],
-        textInputAction: action,
-        style: theme.style,
-        initialValue: '0',
-        decoration: InputDecoration(
-          counterText: '',
-          contentPadding: theme.contentPadding,
-          prefixIcon: Padding(
-            padding: theme.iconPadding,
-            child: Icon(
-              AppIcons.dollarIcon,
-              size: context.sizes.x6,
-              color: theme.iconColor,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AppText.paragraph3(label),
+        SizedBox(height: sizes.x1),
+        SizedBox(
+          width: theme.width,
+          height: theme.height,
+          child: TextFormField(
+            maxLength: 17,
+            onChanged: (value) => onChanged(_stringValueToDouble(value)),
+            keyboardType: TextInputType.number,
+            inputFormatters: [CurrencyTextInputFormatter()],
+            textInputAction: action,
+            style: theme.style,
+            initialValue: '0',
+            decoration: InputDecoration(
+              counterText: '',
+              contentPadding: theme.contentPadding,
+              prefixIcon: Padding(
+                padding: theme.iconPadding,
+                child: Icon(
+                  AppIcons.dollarIcon,
+                  size: sizes.x6,
+                  color: theme.iconColor,
+                ),
+              ),
+              prefixIconConstraints: BoxConstraints.tightFor(height: sizes.x5),
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: theme.borderColor),
+                borderRadius: theme.borderRadius,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: theme.borderColor),
+                borderRadius: theme.borderRadius,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: theme.borderColor),
+                borderRadius: theme.borderRadius,
+              ),
+              errorBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: theme.errorBorderColor),
+                borderRadius: theme.borderRadius,
+              ),
             ),
-          ),
-          prefixIconConstraints: BoxConstraints.tightFor(height: context.tokens.sizes.x5),
-          border: OutlineInputBorder(
-            borderSide: BorderSide(color: theme.borderColor),
-            borderRadius: theme.borderRadius,
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: theme.borderColor),
-            borderRadius: theme.borderRadius,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: theme.borderColor),
-            borderRadius: theme.borderRadius,
-          ),
-          errorBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: theme.errorBorderColor),
-            borderRadius: theme.borderRadius,
+            validator: (value) {
+              if (value == null || value.isEmpty || _stringValueToDouble(value) == 0) {
+                return validatorMessage ?? TextConstants.enterValueGreaterThanZero;
+              }
+              return null;
+            },
           ),
         ),
-        validator: (value) {
-          if (value == null || value.isEmpty || _stringValueToDouble(value) == 0) {
-            return validatorMessage ?? TextConstants.enterValueGreaterThanZero;
-          }
-          return null;
-        },
-      ),
+      ],
     );
   }
 
