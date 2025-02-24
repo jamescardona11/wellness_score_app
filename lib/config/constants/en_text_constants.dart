e2406baff5ef4b9271447e085aa86d5facc7aee8
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-24 07:14:22 -0500

    ✨ Create Rich text header widget

--- lib/config/constants/en_text_constants.dart
+++ lib/config/constants/en_text_constants.dart
@@ -1,3 +1,5 @@
 abstract class EnConstants {
   static const String enterValueGreaterThanZero = 'Enter a value greater than zero';
+  static const String financialWellnessScore = "Let's find out your ";
+  static const String financialWellnessScoreDescription = 'financial wellness score.';
 }
--- lib/ui/features/health_calculator/health_calculator_screen.dart
+++ lib/ui/features/health_calculator/health_calculator_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wellness_score_app/ui/core/theme/theme.dart';
 import 'package:wellness_score_app/ui/widgets/app_bar_widget.dart';
+import 'package:wellness_score_app/ui/widgets/rich_text_header.dart';
 
 class HealthCalculatorScreen extends StatelessWidget {
   const HealthCalculatorScreen({
@@ -8,10 +10,20 @@ class HealthCalculatorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final typography = context.typography;
+    final colors = context.colors;
     return Scaffold(
       appBar: const AppBarWidget(),
-      body: Center(
-        child: Text('New Page'),
+      body: Column(
+        children: [
+          RichTextHeader(
+            span1: "Let's find out your ",
+            span2: 'financial wellness score.',
+          ),
+          Center(
+            child: Text('New Page'),
+          ),
+        ],
       ),
     );
   }
