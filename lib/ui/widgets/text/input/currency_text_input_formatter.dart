1eb6156ffc03aaece0efe0bae59c3e2884f5ea1f
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2025-02-23 21:06:42 -0500

    ✅  Create Unit Test for formatter and improve the formatter v1

--- lib/ui/widgets/text/input/currency_text_input_formatter.dart
+++ lib/ui/widgets/text/input/currency_text_input_formatter.dart
@@ -1,58 +1,32 @@
-import 'dart:math';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 class CurrencyTextInputFormatter extends TextInputFormatter {
-  final NumberFormat format;
-  final bool enableNegative;
+  CurrencyTextInputFormatter();
 
-  CurrencyTextInputFormatter({
-    required this.format,
-    this.enableNegative = true,
-  });
-
-  num _newNum = 0;
-  String _newString = '';
-  bool _isNegative = false;
-
-  void _formatter(String newText) {
-    _newNum = _parseStrToNum(newText);
-    _newString = (_isNegative ? '-' : '') + format.format(_newNum).trim();
-  }
-
-  num _parseStrToNum(String text) {
-    num value = num.tryParse(text) ?? 0;
-    if (format.decimalDigits! > 0) {
-      value /= pow(10, format.decimalDigits!);
-    }
-    return value;
-  }
+  static final NumberFormat format = NumberFormat.currency(
+    symbol: '',
+    decimalDigits: 0,
+  );
 
   @override
   TextEditingValue formatEditUpdate(
     TextEditingValue oldValue,
     TextEditingValue newValue,
   ) {
-    if (enableNegative) {
-      _isNegative = newValue.text.startsWith('-');
-    } else {
-      _isNegative = false;
-    }
-
-    String newText = newValue.text.replaceAll(RegExp('[^0-9]'), '');
-
-    _formatter(newText);
+    final newText = newValue.text.replaceAll(RegExp('[^0-9]'), '');
+    final newString = format.format(num.tryParse(newText) ?? 0).trim();
 
     if (newText.trim().isEmpty || newText == '00' || newText == '000') {
-      return TextEditingValue(
-        text: _isNegative ? '-' : '',
-        selection: TextSelection.collapsed(offset: _isNegative ? 1 : 0),
+      return const TextEditingValue(
+        text: '0',
+        selection: TextSelection.collapsed(offset: 1),
       );
     }
 
     return TextEditingValue(
-      text: _newString,
-      selection: TextSelection.collapsed(offset: _newString.length),
+      text: newString,
+      selection: TextSelection.collapsed(offset: newString.length),
     );
   }
 }
